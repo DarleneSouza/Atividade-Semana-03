c13fa711e3263fd03bdc317e2e2459b7fc9badbd
DarleneSouza <91565165+DarleneSouza@users.noreply.github.com>
2021-10-10 18:20:11 -0300

    Atividade Semana 03

---
+++ Atividade 2 aula sicronia/Atividade 2 aula sicronia/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Atividade_2_aula_sicronia
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.TxtSenha = new System.Windows.Forms.TextBox();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.LblUsuario = new System.Windows.Forms.Label();
+            this.LblSenha = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.Location = new System.Drawing.Point(259, 106);
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(258, 27);
+            this.TxtUsuario.TabIndex = 0;
+            // 
+            // TxtSenha
+            // 
+            this.TxtSenha.Location = new System.Drawing.Point(259, 170);
+            this.TxtSenha.Name = "TxtSenha";
+            this.TxtSenha.Size = new System.Drawing.Size(258, 27);
+            this.TxtSenha.TabIndex = 1;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Location = new System.Drawing.Point(351, 267);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(94, 29);
+            this.BtnLogin.TabIndex = 2;
+            this.BtnLogin.Text = "Login";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // LblUsuario
+            // 
+            this.LblUsuario.AutoSize = true;
+            this.LblUsuario.Location = new System.Drawing.Point(259, 83);
+            this.LblUsuario.Name = "LblUsuario";
+            this.LblUsuario.Size = new System.Drawing.Size(59, 20);
+            this.LblUsuario.TabIndex = 3;
+            this.LblUsuario.Text = "Usuário";
+            // 
+            // LblSenha
+            // 
+            this.LblSenha.AutoSize = true;
+            this.LblSenha.Location = new System.Drawing.Point(259, 147);
+            this.LblSenha.Name = "LblSenha";
+            this.LblSenha.Size = new System.Drawing.Size(49, 20);
+            this.LblSenha.TabIndex = 4;
+            this.LblSenha.Text = "Senha";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblSenha);
+            this.Controls.Add(this.LblUsuario);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.TxtSenha);
+            this.Controls.Add(this.TxtUsuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.TextBox TxtSenha;
+        private System.Windows.Forms.Button BtnLogin;
+        private System.Windows.Forms.Label LblUsuario;
+        private System.Windows.Forms.Label LblSenha;
+    }
+}
+
---
+++ Atividade 2 aula sicronia/Atividade 2 aula sicronia/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade_2_aula_sicronia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string menssagem = "";
+            string usuario = TxtUsuario.Text;
+            string senha = TxtSenha.Text;
+            if (usuario == "admin" && senha == "ifsuldeminas")
+            {
+                menssagem = "Bem vindo!";
+            }
+            else
+            {
+                menssagem = "Acesso negado";
+            }
+            MessageBox.Show(menssagem);
+        }
+    }
+}
